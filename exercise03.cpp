e53c0fac58032a23305a77967709dc9a88fa3722
Mohammed Nuski <99162363+IT21350214@users.noreply.github.com>
2022-02-24 17:45:38 +0000

    Code edited

--- exercise03.cpp
+++ exercise03.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
 
-using namespace std;
 
-int volume(int height, int width, int length);
+int volume(int height; int width; int length);
 
 // 1. Define a structure called Box
 // have the integer data types Height, Width, Length
+struct box{ int height; int width; int length };
 
 // Do not change the main function
 int main() {
     // 2. Create a variable called box1 of the Box structure type
     // int box1Height, box1Width, box1Length;
+  struct box box1 , box2 ;
     // 3. Create a variable called box2 of the Box structure type
     
     // int box2Height, box2Width, box2Length;
     int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
-    cout << "Enter Box 1 Height : ";
-    cin >> Height of Box 1;
-    cout << "Enter Box 1 Width : ";
-    cin >> Width of Box 1;
-    cout << "Enter Box 1 Length : ";
-    cin >> Length of Box 1;
+    std::cout << "Enter Box 1 Height : ";
+    std::cin >> Height of Box 1;
+    std::cout << "Enter Box 1 Width : ";
+    std::cin >> Width of Box 1;
+    std::cout << "Enter Box 1 Length : ";
+    std::cin >> Length of Box 1;
     
-    cout << "Enter Box 2 Height : ";
-    cin >> Height of Box 2;
-    cout << "Enter Box 2 Width : ";
-    cin >> Width of Box 2;
-    cout << "Enter Box 2 Length : ";
-    cin >> Length of Box 2;
+    std::cout << "Enter Box 2 Height : ";
+    std::cin >> Height of Box 2;
+    std::cout << "Enter Box 2 Width : ";
+    std::cin >> Width of Box 2;
+    std::cout << "Enter Box 2 Length : ";
+    std::cin >> Length of Box 2;
     
     // 5. Replace the coding below to pass the Box type structure
-    totalVolume = volume( , , )
-             + volume( , , );
+    totalVolume = volume( box1.height , box1.width , box1.length )
+             + volume( box2.height , box2.width , box2.length );
     
-    cout << "Volume of Box is " << totalVolume << endl;
+    std::cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
 }
 
 // Implement the functions here
+int volume(int height, int width, int length)
+{
+  return height * width * length;
+}

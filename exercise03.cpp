acbd6e76a98b6f76a3c016a5a27748b8535ceb15
Mohammed Nuski <99162363+IT21350214@users.noreply.github.com>
2022-02-27 13:52:24 +0000

    edited not fully error in exercice 3

--- exercise03.cpp
+++ exercise03.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 
 int volume(int height; int width; int length);
 
@@ -18,25 +18,29 @@ int main() {
     int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
-    std::cout << "Enter Box 1 Height : ";
-    std::cin >> Height of Box 1;
-    std::cout << "Enter Box 1 Width : ";
-    std::cin >> Width of Box 1;
-    std::cout << "Enter Box 1 Length : ";
-    std::cin >> Length of Box 1;
+    cout << "Enter Box 1 Height : ";
+    cin >> Height of Box 1;
+      
+    cout << "Enter Box 1 Width : ";
+    cin >> Width of Box 1;
+      
+    cout << "Enter Box 1 Length : ";
+    cin >> Length of Box 1;
     
-    std::cout << "Enter Box 2 Height : ";
-    std::cin >> Height of Box 2;
-    std::cout << "Enter Box 2 Width : ";
-    std::cin >> Width of Box 2;
-    std::cout << "Enter Box 2 Length : ";
-    std::cin >> Length of Box 2;
+    cout << "Enter Box 2 Height : ";
+    cin >> Height of Box 2;
+      
+    cout << "Enter Box 2 Width : ";
+    cin >> Width of Box 2;
+      
+    cout << "Enter Box 2 Length : ";
+    cin >> Length of Box 2;
     
     // 5. Replace the coding below to pass the Box type structure
     totalVolume = volume( box1.height , box1.width , box1.length )
              + volume( box2.height , box2.width , box2.length );
     
-    std::cout << "Volume of Box is " << totalVolume << endl;
+    cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
 }
